39169811b69bed6d84a11d1f0748a590d0a2e3ab
Dev-Amna <amnaharaf272@gmail.com>
2025-12-11 10:33:25 +0500

    Initial commit with project files

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}>
+      <Text style={styles.text}>Contact Manager App</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+})
